a42fb324a4be713833d47323fd0a143236947dce
damien <damien@rooking.co.jp>
2019-06-27 14:26:36 +0900

    test2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 import './App.css';
 import styled from 'styled-components';
-import { placeholder } from '@babel/types';
+import axios from 'axios';
 
 interface IData {
   label: string;
   type: string;
   placeholder: string;
   input: string;
+  value: string;
 }
 
 const App: React.FC = () => {
   const [form, setForm] = useState<IData[]>([]);
   const [source, setSource] = useState('');
 
+  const handleChange = ({ target }: { target: { name: string; value: string } }) => {
+    let updatedForm = [...form];
+    const updatedItem = updatedForm.find(item => item.label === target.name) as IData;
+    updatedItem.value = target.value;
+    setForm(updatedForm);
+  };
+
+  const handleSubmit = () => {
+    console.log('form', form);
+    axios.post('http://localhost:8000/inquiries', form).then(response => console.log(response));
+  };
+
   useEffect(() => {
     const data: IData[] = [
-      { label: 'name', input: 'input', type: 'text', placeholder: 'Enter your name' },
-      { label: 'email', input: 'input', type: 'email', placeholder: 'Enter your email address' },
-      { label: 'message', input: 'textarea', type: 'textarea', placeholder: 'Enter your message' },
+      { label: 'name', input: 'input', type: 'text', placeholder: 'Enter your name', value: '' },
+      { label: 'email', input: 'input', type: 'email', placeholder: 'Enter your email address', value: '' },
+      { label: 'message', input: 'textarea', type: 'textarea', placeholder: 'Enter your message', value: '' },
     ];
 
     const api = new Promise<IData[]>(function(resolve, reject) {
@@ -49,9 +62,12 @@ const App: React.FC = () => {
         {form.map(item => (
           <div key={item.label}>
             <label>{item.label}</label>
-            {React.createElement(item.input, { placeholder: item.placeholder, type: item.type }, null)}
+            {React.createElement(item.input, { name: item.label, value: item.value, placeholder: item.placeholder, type: item.type, onChange: (event: any) => handleChange(event) }, null)}
           </div>
         ))}
+        <button type="button" onClick={() => handleSubmit()}>
+          submit
+        </button>
       </form>
     </Container>
   );
